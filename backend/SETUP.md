492424552465a7e5993129f3e090d599ccb5a398
AwesomenessReborn <hareee234@gmail.com>
2025-04-26 17:55:57 -0700

    updating secrets

--- backend/SETUP.md
+++ backend/SETUP.md
@@ -1,6 +1,15 @@
 # Setup application
 
+### Env setup
 Run the following with conda, to create env: 
 ```
 conda env create -f environment.yml
+```
+
+### Mongo cred setup. 
+Create a `.env` file within the `/backend` folder. Then write your username and password as indicated: 
+
+```
+MONGODB_USERNAME=hari
+MONGODB_PASSWORD=123
 ```
--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,23 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
-uri = "mongodb+srv://hari:123@db.xtn5b1o.mongodb.net/?retryWrites=true&w=majority&appName=db"
+# Load environment variables from .env file
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
+# Get the variables
+username = os.getenv("MONGODB_USERNAME")
+password = os.getenv("MONGODB_PASSWORD")
+
+uri = f"mongodb+srv://{username}:{password}@db.xtn5b1o.mongodb.net/?retryWrites=true&w=majority&appName=db"
+
+# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    print(e) 
