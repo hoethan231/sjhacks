8deb36372b01c7034e1ab9277939c96a6b93912b
hoethan231 <Icydragons1357@gmail.com>
2025-04-26 20:03:42 -0700

    connect to database

--- backend/db.py
+++ backend/db.py
@@ -3,21 +3,14 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+load_dotenv()
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client.get_database('database')
+users = pymongo.collection.Collection(db, 'emergencies')
 
-# Get the variables
-username = os.getenv("MONGODB_USERNAME")
-password = os.getenv("MONGODB_PASSWORD")
-
-uri = f"mongodb+srv://{username}:{password}@db.xtn5b1o.mongodb.net/?retryWrites=true&w=majority&appName=db"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e) 
+    print(e)
+    
