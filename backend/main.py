de4152dc73005e50b39284c54292cda15c7dc18d
AwesomenessReborn <hareee234@gmail.com>
2025-04-27 04:16:58 -0700

    oops

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from routes import emergency, call
 app = FastAPI()
 
 app.include_router(emergency.emergency_router)
-app.include_router(call.router)
+app.include_router(call.call_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
